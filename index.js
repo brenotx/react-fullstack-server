2d0e28c1577912630ac992c88735447840e43945
BrenoTeixeira <brenotx@gmail.com>
2017-10-21 09:36:45 -0200

    Enabling cookies

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const passport = require('passport');
+const cookieSession = require('cookie-session');
 const keys = require('./config/keys');
 
 require('./models/User');
@@ -8,6 +10,17 @@ require('./services/passport');
 mongoose.connect(keys.mongoURI, { useMongoClient: true });
 
 const app = express();
+
+app.use(
+    cookieSession({
+        // name: 'session',
+        keys: [keys.cookieKey],
+        maxAge: 30 * 24 * 60 * 60 * 1000
+    })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 require('./routes/authRoutes')(app);
 
