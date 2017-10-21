518fa7d211332a41fca41d297c94d3399f3287f7
BrenoTeixeira <brenotx@gmail.com>
2017-10-21 08:34:41 -0200

    Saving model instances

--- services/passport.js
+++ services/passport.js
@@ -1,6 +1,9 @@
+const mongoose = require('mongoose');
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const keys = require('../config/keys');
+
+const Users = mongoose.model('users');
 
 passport.use(
     new GoogleStrategy(
@@ -10,9 +13,7 @@ passport.use(
             callbackURL: '/auth/google/callback'
         },
         (accessToken, refreshToken, profile, cb) => {
-            console.log(accessToken);
-            console.log(refreshToken);
-            console.log(profile);
+            new Users({ googleId: profile.id }).save();
             // User.findOrCreate({ googleId: profile.id }, function(err, user) {
             //     return cb(err, user);
             // });
